ee18f7cebdbd39a09e7d17b9d33f4b9e46cd6499
Matthew Novelli <mattnovelli@gmail.com>
2025-05-07 14:54:22 -0500

    fix json import

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import redirects from './redirects.json' assert { type: 'json' };
+import redirects from './redirects.json' with { type: 'json' };
 
 const nextConfig = {
 	async redirects() {
